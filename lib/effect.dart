8202c58dcd67143c04ed46dffd8297de868f543a
Goddchen <goddchen@gmail.com>
2024-02-21 21:48:34 +0100

    ci: add basic GitHub Actions (#1)
    * ci: add basic GitHub Actions
    
    * ci: add dummy test
    
    * ci: add dummy test
    
    * ci: add dummy test

--- lib/effect.dart
+++ lib/effect.dart
@@ -1,2 +1,5 @@
 /// effect
 library;
+
+/// Remove once real code and tests are available
+void dummyForTest() {}
---
+++ test/dummy_test.dart
@@ -0,0 +1,6 @@
+import 'package:effect/effect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  test('Remove once real tests are available', dummyForTest);
+}
